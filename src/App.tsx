96f692299e249f5d9e69467190df90f1fa9af5d3
TajBanana <ytahjian@gmail.com>
2023-09-24 16:44:18 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+  alert('everything is ok')
+  return (
+      <>
+        <div> WebGL</div>
+        <canvas></canvas>
+      </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div> WebGL</div>
+    <canvas></canvas>
+  </React.StrictMode>,
+)
